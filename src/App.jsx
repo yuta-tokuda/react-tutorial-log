098e3f01db65505e01502ce65f5c3da5309ebef9
徳田裕太 <yuta@MacBook-Pro.local>
2025-12-30 23:16:02 +0900

    #4 イベントハンドラ

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// イベントに応答する (https://ja.react.dev/learn#responding-to-events)
+// ボタン用コンポーネント
+function MyButton() {
+  // ボタンがクリックされた時の処理
+  function handleClick() {
+    alert('You clicked me!');
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <button onClick={handleClick}>
+      Click me
+    </button>
+  );
 }
 
-export default App
+export default function App() {
+  return (
+    <div>
+      <MyButton />
+    </div>
+  );
+}
