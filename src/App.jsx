db5148279d170d13c642a9297fd71b54ace400dc
徳田裕太 <yuta@MacBook-Pro.local>
2026-01-03 22:36:39 +0900

    #7 イベントハンドラとprops

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// 1.ただのボタン
+// export default function Button() {
+//   return (
+//     <button>
+//       I don't do anything
+//     </button>
+//   );
+// }
 
-function App() {
-  const [count, setCount] = useState(0)
+// 2.ボタンクリックでアラート表示
+// export default function Button() {
+//   function handleClick() {
+//     alert('You clicked me!');
+//   }
 
+//   return (
+//     <button onClick={handleClick}>
+//       Click me
+//     </button>
+//   );
+// }
+
+
+// 3.ボタンクリックでアラート表示(ボタン複数・propsを使用)
+// function AlertButton({ message, children }) {
+//   return (
+//     <button onClick={() => alert(message)}>
+//       {children}
+//     </button>
+//   );
+// }
+
+// export default function Toolbar() {
+//   return (
+//     <div>
+//       <AlertButton message="Playing!">
+//         Play Movie
+//       </AlertButton>
+//       <AlertButton message="Uploading!">
+//         Upload Image
+//       </AlertButton>
+//     </div>
+//   );
+// }
+
+// 4.イベントハンドラを props として渡す
+function Button({ onClick, children }) {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <button onClick={onClick}>
+      {children}
+    </button>
+  );
 }
 
-export default App
+function PlayButton({ movieName }) {
+  function handlePlayClick() {
+    alert(`Playing ${movieName}!`);
+  }
+
+  return (
+    <Button onClick={handlePlayClick}>
+      Play "{movieName}"
+    </Button>
+  );
+}
+
+function UploadButton() {
+  return (
+    <Button onClick={() => alert('Uploading!')}>
+      Upload Image
+    </Button>
+  );
+}
+
+export default function Toolbar() {
+  return (
+    <div>
+      <PlayButton movieName="Kiki's Delivery Service" />
+      <UploadButton />
+    </div>
+  );
+}
