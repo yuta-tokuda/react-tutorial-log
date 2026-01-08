b8d382ee8ca9a591b950f9872311f0566b4794b1
徳田裕太 <yuta@MacBook-Pro.local>
2026-01-08 14:57:34 +0900

    #8 バブリング

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// イベントの伝播
+// export default function Toolbar() {
+//   return (
+//     <div className="Toolbar" onClick={() => {
+//       alert('You clicked on the toolbar!');
+//     }}>
+//       <button onClick={() => alert('Playing!')}>
+//         Play Movie
+//       </button>
+//       <button onClick={() => alert('Uploading!')}>
+//         Upload Image
+//       </button>
+//     </div>
+//   );
+// }
 
-function App() {
-  const [count, setCount] = useState(0)
-
+// 伝播の停止
+function Button({ onClick, children }) {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <button onClick={e => {
+      e.stopPropagation();
+      onClick();
+    }}>
+      {children}
+    </button>
+  );
 }
 
-export default App
+export default function Toolbar() {
+  return (
+    <div className="Toolbar" onClick={() => {
+      alert('You clicked on the toolbar!');
+    }}>
+      <Button onClick={() => alert('Playing!')}>
+        Play Movie
+      </Button>
+      <Button onClick={() => alert('Uploading!')}>
+        Upload Image
+      </Button>
+    </div>
+  );
+}
