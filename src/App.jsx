5883a2a8a06eb546f35e7f9b7a7a397279f10889
徳田裕太 <yuta@MacBook-Pro.local>
2026-01-12 11:34:34 +0900

    #9 state：コンポーネントのメモリ

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,113 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// 動かない例
+// import { sculptureList } from './data.js';
 
-function App() {
-  const [count, setCount] = useState(0)
+// export default function Gallery() {
+//   let index = 0;
 
+//   function handleClick() {
+//     index = index + 1;
+//   }
+
+//   let sculpture = sculptureList[index];
+//   return (
+//     <>
+//       <button onClick={handleClick}>
+//         Next
+//       </button>
+//       <h2>
+//         <i>{sculpture.name} </i> 
+//         by {sculpture.artist}
+//       </h2>
+//       <h3>  
+//         ({index + 1} of {sculptureList.length})
+//       </h3>
+//       <img 
+//         src={sculpture.url} 
+//         alt={sculpture.alt}
+//       />
+//       <p>
+//         {sculpture.description}
+//       </p>
+//     </>
+//   );
+// }
+
+// 動く例
+// import { useState } from 'react';
+// import { sculptureList } from './data.js';
+
+// export default function Gallery() {
+//   const [index, setIndex] = useState(0);
+
+//   function handleClick() {
+//     setIndex(index + 1);
+//   }
+
+//   let sculpture = sculptureList[index];
+//   return (
+//     <>
+//       <button onClick={handleClick}>
+//         Next
+//       </button>
+//       <h2>
+//         <i>{sculpture.name} </i> 
+//         by {sculpture.artist}
+//       </h2>
+//       <h3>  
+//         ({index + 1} of {sculptureList.length})
+//       </h3>
+//       <img 
+//         src={sculpture.url} 
+//         alt={sculpture.alt}
+//       />
+//       <p>
+//         {sculpture.description}
+//       </p>
+//     </>
+//   );
+// }
+
+// 複数のstate
+import { useState } from 'react';
+import { sculptureList } from './data.js';
+
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  // 初期値をfalseで定義する
+  const [showMore, setShowMore] = useState(false);
+
+  function handleNextClick() {
+    setIndex(index + 1);
+  }
+
+  // toggleボタンのイベント用
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  let sculpture = sculptureList[index];
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={handleNextClick}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i> 
+        by {sculpture.artist}
+      </h2>
+      <h3>  
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {/* 値によって表示する文字を変化させている */}
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      {/* もし表示状態(値がtrue)であれば要素を表示する*/}
+      {showMore && <p>{sculpture.description}</p>}
+      <img 
+        src={sculpture.url} 
+        alt={sculpture.alt}
+      />
     </>
-  )
+  );
 }
-
-export default App
